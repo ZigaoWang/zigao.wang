3103bd3dd5de1ff8d665e1c80f57a30c144a3f71
Zigao Wang <zigao.wang24@gmail.com>
2026-05-10 14:17:05 +0800

    feat: improve SEO with robots.txt, OG images, and better meta tags

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "Zigao's Blog";
-export const SITE_DESCRIPTION = "Zigao's personal dev blog";
+export const SITE_DESCRIPTION = "A high school developer writing about indie apps, books, talks, and whatever else is interesting.";
